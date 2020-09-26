f05bc302f7b54fd0390e03a7e2294b034fa50f08
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2020-09-27 01:41:52 +0200

    Solved the Double character program

--- Problems/Double characters/src/Main.java
+++ Problems/Double characters/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+/*
+Double characters
+Write a program that reads a string and then output another string with doubled characters.
+Sample Input:
+The
+Sample Output:
+TThhee
+ */
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        StringBuilder finale = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            finale.append(input.charAt(i)).append(input.charAt(i));
+        }
+        System.out.println(finale);
     }
 }
