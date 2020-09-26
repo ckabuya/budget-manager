9ee31f3e7aab5f9a8830cd118aa95283786f90ce
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2020-09-27 01:56:49 +0200

    Solved the Palindrome program

--- Problems/Palindrome/src/Main.java
+++ Problems/Palindrome/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+/*
+Problem
+Write a program that reads a string and checks whether it is a palindrome, i.e. it reads the same both left-to-right and right-to-left.
+The program must output “yes” if the string is a palindrome and “no” otherwise.
+Sample Input:
+kayak
+Sample Output:
+yes
+ */
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        StringBuilder builder = new StringBuilder(input);
+        String reversed = builder.reverse().toString();
+        if (reversed.equalsIgnoreCase(input)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
     }
 }
