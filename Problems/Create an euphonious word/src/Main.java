cb65dcb7be7792638ea36c562f0edf9a344234b5
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2020-10-03 00:32:08 +0200

    Solved Create an euphonious word

--- Problems/Create an euphonious word/src/Main.java
+++ Problems/Create an euphonious word/src/Main.java
@@ -2,8 +2,27 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
-        String CONSONANTS = "bcdfghjklmnpqrstvwxz";
+        // write your code here;
         String VOWELS = "aeiouy";
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        //help from https://stackoverflow.com/questions/63671303/create-a-euphonious-word
+        int count = 0;
+        int currentTypeCount = 0;
+        boolean lastTypeVowel = true;
+        char[] word = input.toCharArray();
+        for (char c : word) {
+            if ((VOWELS.indexOf(c) != -1) == lastTypeVowel) {
+                if (++currentTypeCount == 3) {
+                    count++;
+                    currentTypeCount = 1;
+                }
+            } else {
+                lastTypeVowel = !lastTypeVowel;
+                currentTypeCount = 1;
+            }
+        }
+        System.out.println(count);
     }
+
 }
