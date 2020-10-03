8bb5795b897a20fe125d39da5010abc90399f5c0
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2020-10-03 02:04:17 +0200

    Complete Budget manager Stage 1/5: Count my money

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -1,7 +1,25 @@
 package budget;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        String[] data;
+        double total = 0.0;
+
+        StringBuilder output = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            data = input.split("\\$");
+
+            if (!input.isBlank()) {
+                total += Float.parseFloat(data[1]);
+                output.append(input).append("\n");
+            }
+        }
+        System.out.println(output.toString());
+        System.out.printf("Total: $%f", total);
     }
 }
