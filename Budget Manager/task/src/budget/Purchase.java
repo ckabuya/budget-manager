44d8a06f18891198c7296f176c15e149203e28f0
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2020-10-05 16:25:59 +0200

    Starting Budget manager Stage 3/5: Oh the things you can buy. Updated Purchase.java with category variable

--- Budget Manager/task/src/budget/Purchase.java
+++ Budget Manager/task/src/budget/Purchase.java
@@ -5,12 +5,17 @@ import java.util.Objects;
 public class Purchase {
     private double price;
     private String name;
+    private String category;
 
     public Purchase(double price, String name) {
         this.price = price;
         this.name = name;
     }
-
+    public Purchase(double price, String name, String category) {
+        this.price = price;
+        this.name = name;
+        this.category =category;
+    }
     public Purchase() {
     }
 
@@ -30,17 +35,35 @@ public class Purchase {
         this.name = name;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Purchase)) return false;
         Purchase purchase = (Purchase) o;
         return Double.compare(purchase.getPrice(), getPrice()) == 0 &&
-                Objects.equals(getName(), purchase.getName());
+                Objects.equals(getName(), purchase.getName()) &&
+                Objects.equals(getCategory(), purchase.getCategory());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getPrice(), getName());
+        return Objects.hash(getPrice(), getName(), getCategory());
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "price=" + price +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                '}';
     }
 }
