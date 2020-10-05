a5c8d7c332a27c92d0c09b64d4d8bb721366543c
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2020-10-05 09:45:32 +0200

    Starting Stage 2/5: Make a menu

--- Problems/Palindrome/src/Main.java
+++ Problems/Palindrome/src/Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 /*
-Problem
-Write a program that reads a string and checks whether it is a palindrome, i.e. it reads the same both left-to-right and right-to-left.
+Write a program that reads a string and checks whether it is a palindrome, 
+i.e. it reads the same both left-to-right and right-to-left.
 The program must output “yes” if the string is a palindrome and “no” otherwise.
 Sample Input:
 kayak
