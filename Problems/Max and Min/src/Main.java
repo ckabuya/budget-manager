2be531505429847ece6916d70969687c4a01e1a5
Chrispin Kabuya <chrispin.kabuya@gmail.com>
2020-11-22 19:02:48 +0200

    Solved Strategy Pattern problem

---
+++ Problems/Max and Min/src/Main.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+class Finder {
+
+    private FindingStrategy strategy;
+
+    public Finder(FindingStrategy strategy) {
+        // write your code here
+        this.strategy = strategy;
+    }
+
+    /**
+     * It performs the search algorithm according to the given strategy
+     */
+    public int find(int[] numbers) {
+        // write your code here
+        return this.strategy.getResult(numbers);
+    }
+}
+
+interface FindingStrategy {
+
+    /**
+     * Returns search result
+     */
+    int getResult(int[] numbers);
+
+}
+
+class MaxFindingStrategy implements FindingStrategy {
+
+    public int getResult(int[] numbers) {
+        int result;
+        // write your code here
+        if (numbers == null || numbers.length == 0) {
+            result = Integer.MIN_VALUE;
+        } else {
+            int max = numbers[0];
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] > max) {
+                    max = numbers[i];
+                }
+            }
+            result = max;
+        }
+        return result;
+    }
+}
+
+class MinFindingStrategy implements FindingStrategy {
+
+    public int getResult(int[] numbers) {
+        int result;
+        // write your code here
+        if (numbers == null || numbers.length == 0) {
+            result = Integer.MAX_VALUE;
+        } else {
+            int min = numbers[0];
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] < min) {
+                    min = numbers[i];
+                }
+            }
+            result = min;
+        }
+        return result;
+    }
+}
+
+/* Do not change code below */
+public class Main {
+
+    public static void main(String[] args) {
+
+        final Scanner scanner = new Scanner(System.in);
+
+        final String[] elements = scanner.nextLine().split("\\s+");
+        int[] numbers = null;
+
+        if (elements[0].equals("EMPTY")) {
+            numbers = new int[0];
+        } else {
+            numbers = new int[elements.length];
+            for (int i = 0; i < elements.length; i++) {
+                numbers[i] = Integer.parseInt(elements[i]);
+            }
+        }
+
+        final String type = scanner.nextLine();
+
+        Finder finder = null;
+
+        switch (type) {
+            case "MIN":
+                finder = new Finder(new MinFindingStrategy());
+                break;
+            case "MAX":
+                finder = new Finder(new MaxFindingStrategy());
+                break;
+            default:
+                break;
+        }
+
+        if (finder == null) {
+            throw new RuntimeException(
+                    "Unknown strategy type passed. Please, write to the author of the problem.");
+        }
+
+        System.out.println(finder.find(numbers));
+    }
+}
